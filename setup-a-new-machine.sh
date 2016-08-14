4b5da9a5251c7721446fcf2e24c2ca8061cb9dca
David De Groot <davidrossdegroot@gmail.com>
2016-08-13 22:15:52 -0400

    Few updates

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -12,5 +12,22 @@ cp .bash_profile ~/.bash_profile
 cp .aliases ~/.aliases
 cp -R fish/ ~/fish
 cp .functions ~/.functions
+cp .exports ~/.exports
+cp .dircolors ~/.dircolors
+cp .gitconfig ~/.gitconfig
+
+# Type `git open` to open the GitHub page or website for a repository.
+npm install -g git-open
+
+# fancy listing of recent branches
+npm install -g git-recent
+
+# sexy git diffs
+npm install -g diff-so-fancy
+
+# trash as the safe `rm` alternative
+npm install --global trash-cli
+
+
 
 source ~/.bash_profile
