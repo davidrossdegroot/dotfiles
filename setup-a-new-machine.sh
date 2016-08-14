4c1b59263a249948f7c1adcaed237e437c1a7adc
David De Groot <davidrossdegroot@gmail.com>
2016-08-13 22:07:49 -0400

    More stuff

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -4,10 +4,13 @@
 git clone git://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_basic_vimrc.sh
 
+# copy the files I want over to home directory. Symlinks maybe better option, but don't have time (or want) to research right now.
+
 cp vim_runtime/my_configs.vim ~/vim_runtime/my_configs.vim
-
 cp .bash_prompt ~/.bash_prompt
-
 cp .bash_profile ~/.bash_profile
+cp .aliases ~/.aliases
+cp -R fish/ ~/fish
+cp .functions ~/.functions
 
 source ~/.bash_profile
