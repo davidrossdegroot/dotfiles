7a9004774996e8ee969d9808e3e614e6dad28180
David De Groot <david.degroot@softtechconsulting.com>
2020-04-23 16:27:47 -0400

    Update setup-a-new-machine.sh

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -1,5 +1,94 @@
-# Assumes the user has cloned or downloaded the following repo https://github.com/davidrossdegroot/dotfiles and is in the root directory
-# when this file is run. 
+# pick out what you want to do from here
+
+#- [ ] Install chrome — login and do the sync
+#- [ ] Download xcode from App Store
+#- [ ] Change battery to show percentage 
+#- [ ] Removed apps from dock that I don’t use. make dock nicer add terminal to dock etc.
+#- [ ] Change mouse scroll direction
+#- [ ] Change mouse to touch to click
+
+#install homebrew https://brew.sh/ 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+# GNU `sed`, overwriting the built-in `sed`
+brew install gnu-sed
+# note to self -- update aliases with sed='gsed'
+
+# Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew install bash
+
+brew install bash-completion
+
+# Install wget with IRI support
+brew install wget
+
+# Install more recent versions of some OS X tools
+brew install vim --with-override-system-vi 
+brew install nano
+brew install grep
+brew install openssh
+brew install screen
+
+# z hopping around folders
+brew install z
+
+# run this script when this file changes guy.
+brew install entr
+
+# github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
+brew install hub
+
+
+# mtr - ping & traceroute. best.
+brew install mtr
+
+# Install other useful binaries
+brew install the_silver_searcher
+brew install fzf # uses ctrl+r for command history searching
+
+brew install git
+brew install imagemagick # this one i might need to do an alias for
+brew install node # This installs `npm` too using the recommended installation method
+brew install pv
+brew install rename
+brew install tree
+brew install ranger
+brew install zopfli
+brew install ffmpeg # prob don't need this in the future
+
+brew install terminal-notifier
+brew install pidcat   # colored logcat guy
+
+brew install ncdu # find where your diskspace went
+
+brew install zsh
+
+# Remove outdated versions from the cellar
+brew cleanup
+
+# Install native apps
+
+brew cask install spectacle
+brew cask install gyazo
+brew cask install 1password
+
+# dev
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install visual-studio-code
+
 
 git clone git://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_basic_vimrc.sh
