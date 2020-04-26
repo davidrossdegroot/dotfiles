f78f76f52cfd0e33ab93f18b30feabe969081d9c
David De Groot <david.degroot@softtechconsulting.com>
2020-04-26 16:21:50 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -73,6 +73,19 @@ brew install pidcat   # colored logcat guy
 
 brew install ncdu # find where your diskspace went
 
+# nvm 
+brew uninstall --ignore-dependencies node
+brew uninstall --force node
+brew install nvm
+nvm install node # gets latest stable version
+
+# rvm 
+brew install gpg
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+\curl -sSL https://get.rvm.io | bash -s stable --ruby  # get latest stable ruby
+
 brew install zsh
 
 # these are all the brew cask installs I did.. not sure I need all of them.
