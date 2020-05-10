7a4d8e6ab98f6538c8bd1ee4802006b7075ae4a4
David De Groot <davidrossdegroot@gmail.com>
2020-05-09 20:43:16 -0400

    pull request

--- pull-request.sh
+++
@@ -1,5 +0,0 @@
-repo=`git remote -v | grep -m 1 "(push)" | sed -e 's/.*DIDIT//' -e 's/.git.*//' | sed 's/.//'`
-branch=`git name-rev --name-only HEAD`
-compareTo="master"
-echo "... creating pull request for branch \"$branch\" and comparing to \"$compareTo\" in \"$repo\""
-open https://git.uscis.dhs.gov/DIDIT/$repo/compare/$compareTo...$branch
