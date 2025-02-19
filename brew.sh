865a8501002062e8c5a137bd2b9f6cf9000946c6
David De Groot <davidrossdegroot@gmail.com>
2025-02-19 11:13:45 -0500

    brew sh was way bloated

--- brew.sh
+++ brew.sh
@@ -37,9 +37,6 @@ brew install homebrew/dupes/screen
 # z hopping around folders
 brew install z
 
-# run this script when this file changes guy.
-brew install entr
-
 # github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
 brew install hub
 
@@ -47,44 +44,17 @@ brew install hub
 # mtr - ping & traceroute. best.
 brew install mtr
 
-    # allow mtr to run without sudo
-    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
-    sudo chmod 4755 $mtrlocation/sbin/mtr
-    sudo chown root $mtrlocation/sbin/mtr
-
-
-# Install other useful binaries
-brew install the_silver_searcher
 brew install fzf
 
 brew install git
 brew install imagemagick --with-webp
-brew install node # This installs `npm` too using the recommended installation method
-brew install pv
-brew install rename
 brew install tree
-brew install zopfli
-brew install ffmpeg --with-libvpx
-
-brew install terminal-notifier
-
-brew install android-platform-tools
-brew install pidcat   # colored logcat guy
-
-brew install ncdu # find where your diskspace went
 
 # nvm 
 brew uninstall --ignore-dependencies node
 brew uninstall --force node
 brew install nvm
 nvm install node # gets latest stable version
-
-# rvm 
-brew install gpg
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
-\curl -sSL https://get.rvm.io | bash -s stable --ruby  # get latest stable ruby
 
 brew install zsh
 
